99930fba27144f1c2827d63c6d881a228fbb9671
Md Sahil <md61421@gmail.com>
2024-01-05 20:14:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const generateBtn = document.querySelector("#generate__btn");
+const copyBtn = document.querySelector("#copy__btn");
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+const createPassword = () => {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passwordBox.value = password;
+};
+
+const copyPassword = () => {
+    password.select();
+    document.execCommand("copy");
+}
+
+generateBtn.addEventListener("click", createPassword);
+copyBtn.addEventListener("click", copyPassword);
